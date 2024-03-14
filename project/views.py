486b5883f4b2d9d9ef400edb66d870200f7389de
susanchelsea <susanchelsian@gmail.com>
2024-03-14 15:15:28 +0545

    Create page done

--- project/views.py
+++ project/views.py
@@ -8,4 +8,12 @@ def home(request):
     return render(request, 'home.html', context=data)
 
 def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        description = request.POST.get('description')
+        ingredients = request.POST.get('ingredients')
+        process = request.POST.get('process')
+        picture = request.FILES.get('picture')
+        Recipe.objects.create(name=name,category=category,description=description,ingredients=ingredients,process=process,picture=picture)
     return render(request, 'create.html')
